106d1bb0bf23dcb9e618ff0f53c2b10e687587d4
Heiz <revigr97@gmail.com>
2022-03-07 13:39:43 +0800

    feat: 登录页

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+const webpack = require('webpack')
 module.exports = {
   configureWebpack: (config) => {
     config.plugins.push(
@@ -14,5 +19,33 @@ module.exports = {
         })
       ]
     )
+  },
+  chainWebpack(config) {
+    config.module.rule('svg').exclude.add(resolve('src/icons')).end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+    config
+      .plugin('ignore')
+      .use(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn$/))
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
   }
 }
